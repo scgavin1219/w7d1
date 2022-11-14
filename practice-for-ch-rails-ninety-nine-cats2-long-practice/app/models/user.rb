70934be8bee5703260f4a2afd816723868d3ec5b
AA Student <appacademy@Mini-15.local>
2022-11-14 15:30:18 -0800

    first commit

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice/app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :username, uniqueness: true
+    validates :password_digest, :username, :session_token, presence: true
+    before_validation :generate_unique_session_token
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && @user.is_password?(password)
+            return @user
+        else
+            return nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        bcrypt_object = BCrypt::Password.new(self.password_digest)
+        bcrypt_object.is_password?(password)
+    end
+
+    def reset_session_token
+        
+    end
+
+    def generate_unique_session_token
+
+    end
+
+    def ensure_session_token
+
+    end
+end
